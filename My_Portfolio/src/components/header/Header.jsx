e40e099715e4cf06fb43e0205d0150f2f6d0bf58
xanj16 <146350155+xanj16@users.noreply.github.com>
2024-01-25 10:08:06 +0800

    pagbago2

--- My_Portfolio/src/components/header/Header.jsx
+++ My_Portfolio/src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Xandra Josh</h1>
-        <h5 className="text-light">Full-Stack Developer</h5>
+        <h5 className="text-light">Full-Stack Cutie</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
           i-scroll mo
